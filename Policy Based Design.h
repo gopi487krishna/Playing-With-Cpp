204ba12bd3ec2a6a6401a6565edbec87326cd4e1
Alpha-BLUE487 <alphablue487@outlook.com>
2020-03-21 09:59:06 +0530

    Added Policy Based Design Example

---
+++ Policy Based Design.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <iostream>
+
+class Widget {};
+
+template<typename T>
+class OPNewCreator {
+
+public:
+	static T* create() {
+		std::cout << "OPNewCreate::Create" << "\n";
+		return new T;
+	}
+
+};
+
+
+template<typename T>
+class MallocCreator {
+public:
+	static T* create() {
+
+		std::cout << "MallocCreator::Create" << "\n";
+		void* rawbuffer = std::malloc(sizeof(T));
+		if (!rawbuffer) {
+			return nullptr;
+		}
+		return new (rawbuffer) T;
+	}
+
+};
+
+template<typename T>
+class PrototypeBasedCreator {
+
+	static T* ptr_prototype_obj = nullptr;
+public:
+	PrototypeBasedCreator(T* ptr_proto = nullptr) :ptr_prototype_obj(ptr_proto) {}
+	static T* getPrototype() {
+		return ptr_prototype_obj;
+	}
+	static void setPrototype(T* ptr_proto) {
+		ptr_prototype_obj = ptr_proto;
+	}
+	static T* create() {
+		std::cout << "PrototypeBasedCreator::Create" << "\n";
+		ptr_prototype_obj->clone();
+	}
+};
+
+template< class Policy>
+class WidgetManager :Policy {
+public:
+	void doSomething() {
+
+		std::cout << "WidgetManager::doSomething()";
+		create();
+	}
+};
+
+
+
